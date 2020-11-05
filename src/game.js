418fb95568aa20710200c3d8f7b172b739268962
Alice Ruppert <aliceruppert@gmail.com>
2020-11-05 16:58:37 -0700

    Pseudocoding for Game

---
+++ src/game.js
@@ -0,0 +1,23 @@
+Set up game class
+- constructor
+  - properties
+    - id
+     - for saving
+    - board (object)
+      - for displaying
+    -isWon
+      -for reset
+  -methods
+    - setupGame
+      - creates new players
+      - sets up board
+    - checkWin
+      - returns winner
+    - assignWin(player)
+      - adds game to their winArray
+    - save to localStorage
+    - add to localStorage
+    - displayGame
+      - possibly creates the display
+  }
+}
