46be08135697a0a8f3c51b0baa7aae58ffa5fe35
Alice Ruppert <aliceruppert@gmail.com>
2020-11-05 20:24:25 -0700

    Build basic Game structure

--- src/game.js
+++ src/game.js
@@ -1,23 +1,53 @@
-Set up game class
-- constructor
-  - properties
-    - id
-     - for saving
-    - board (object)
-      - for displaying
-    -isWon
-      -for reset
-  -methods
-    - setupGame
-      - creates new players
-      - sets up board
-    - checkWin
-      - returns winner
-    - assignWin(player)
-      - adds game to their winArray
-    - save to localStorage
-    - add to localStorage
-    - displayGame
-      - possibly creates the display
+class Game {
+  constructor() {
+    this.id = Date.now();
+    this.isWon = false;
+    this.board = {
+      a1: '',
+      a2: '',
+      a3: '',
+      b1: '',
+      b2: '',
+      b3: '',
+      c1: '',
+      c2: '',
+      c3: '',
+   }
   }
+
+  checkWin(player) {
+    var markerCheck = player.marker + player.marker + player.marker
+    var winCondition =
+      (this.board.a1 + this.board.a2 + this.board.a3 === markerCheck) ||
+      (this.board.b1 + this.board.b2 + this.board.b3 === markerCheck) ||
+      (this.board.c1 + this.board.c2 + this.board.c3 === markerCheck) ||
+      (this.board.a1 + this.board.b2 + this.board.c3 === markerCheck) ||
+      (this.board.a3 + this.board.b2 + this.board.c1 === markerCheck)
+
+    if (winCondition) {
+      return `${player} Wins!`
+      player.wins.push(this)
+    }
+  }
+
+  display() {
+
+  }
+
+  setUpGame() {
+    xPlayer = new Player('X')
+    oPlayer = new Player('O')
+
+
+  }
+
+  // saveToStorage() {
+  //   JSON.setitem(`${this.id}`, JSON.stringify(this))
+  // }
+  //
+  // loadFromStorage(object) {
+  //   this.id = object.id
+  //   this.winner = object.winner
+  //   this.board =
+  // }
 }
