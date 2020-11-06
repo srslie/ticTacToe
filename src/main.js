0af22fe7d586f88d56bc2f34c94f0d7deca43772
Alice Ruppert <aliceruppert@gmail.com>
2020-11-05 22:02:42 -0700

    Set up queryselectors, eventListeners, click move and load start

--- src/main.js
+++ src/main.js
@@ -1,13 +1,41 @@
-set up querySelector for gameBoard
-addEventListener for any clicks on gameBoard
+var gameBoard = document.querySelector('.game-board')
+var space = document.querySelector('.space')
+var broadcast = document.querySelector('h2')
+var xScores = document.querySelector('.x-scores')
+var oScores = document.querySelector('.o-scores')
 
-set up querySelector for space
 
-addEventListener for window Load
+var currentGame
 
-on click, run play function
-- let the Player make a move
+window.addEventListener('load', start)
+gameBoard.addEventListener('click', move)
 
-on load, run load function
-- start a new Game
-- load player scoreBoards
+function move() {
+  broadcast.innerText = ''
+  var targetSpace = event.target.closest('.space')
+  if (currentGame.isXturn) {
+    var x = currentGame.players.xPlayer
+    x.mark(targetSpace, currentGame)
+  } else {
+    currentGame.players.oPlayer.mark(targetSpace, currentGame)
+  }
+  displayScores()
+}
+
+function displayScores() {
+  currentGame.players.xPlayer.displayWins(xScores)
+  currentGame.players.oPlayer.displayWins(oScores)
+
+}
+function start() {
+  //function loadFromStorage
+  currentGame = new Game
+}
+
+
+
+/*
+function loadFromStorage() {
+display in sidebars player.wins game.displays
+
+*/
